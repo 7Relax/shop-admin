815cf30bf025a3e15b5e3c62e179c08b1f0a5af5
seven <lxw_623@163.com>
2022-11-14 12:20:21 +0800

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    // 'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
+    // 'plugin:vue/vue3-strongly-recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+}
