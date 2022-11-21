d185a7ee9f2db01f31563cedd7bdd0f163539a60
seven <lxw_623@163.com>
2022-11-21 15:16:43 +0800

    feat: 进度条nprogress

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,8 @@ import productRoutes from './modules/product'
 import orderRoutes from './modules/order'
 import permissionRoutes from './modules/permission'
 import mediaRoutes from './modules/media'
+import nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 // 路由规则
 const routes: RouteRecordRaw[] = [
@@ -38,6 +40,16 @@ const router = createRouter({
   history: createWebHashHistory(),
   // 路由规则
   routes
+})
+
+// 全局前置守卫：所有页面的导航都会经过这里
+router.beforeEach(() => {
+  nprogress.start() // 开始加载进度条
+})
+
+// 全局后置钩子
+router.afterEach(() => {
+  nprogress.done() // 结束进度条
 })
 
 export default router
